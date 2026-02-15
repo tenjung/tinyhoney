6c3309ab248c086c020d178bfe03ec96cc600ecf
tenjung <doriclan@naver.com>
2026-02-15 23:46:12 +0900

    Fix: Remove redundant SolidQueue rename migrations

--- db/migrate/20260215231206_rename_solid_queue_process_kind_to_name.rb
+++
@@ -1,5 +0,0 @@
-class RenameSolidQueueProcessKindToName < ActiveRecord::Migration[7.1]
-  def change
-    rename_column :solid_queue_processes, :kind, :name
-  end
-end
--- db/migrate/20260215234000_revert_solid_queue_kind_name.rb
+++
@@ -1,5 +0,0 @@
-class RevertSolidQueueKindName < ActiveRecord::Migration[7.1]
-  def change
-    rename_column :solid_queue_processes, :name, :kind
-  end
-end
